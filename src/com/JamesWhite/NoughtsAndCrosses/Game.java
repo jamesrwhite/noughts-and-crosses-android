238595b7a4ae621b1055877f5738b5c1ea481bbc
jameswhite <jameswhite@81d7e092-4b5a-4a12-8c2c-2554addf96e1>
2011-12-27 13:51:20 +0000

    Added some additional methods to the Game class to handle game status and starting the game.
    git-svn-id: file:///Users/jameswhite/Dropbox/Dev/Java/Uni/assignment/branches/Assignment@5 81d7e092-4b5a-4a12-8c2c-2554addf96e1

--- src/com/JamesWhite/NoughtsAndCrosses/Game.java
+++ src/com/JamesWhite/NoughtsAndCrosses/Game.java
@@ -3,9 +3,13 @@ package com.JamesWhite.NoughtsAndCrosses;
 public class Game {
 	
 	private int score;
+	private int status;
+		private final int ACTIVE = 1;
+		private final int WAITING = 2;
+		private final int FINISHED = 0;
 
 	/**
-	 * @return the score
+	 * @return the game score
 	 */
 	public int getScore() {
 		
@@ -14,12 +18,41 @@ public class Game {
 	}
 
 	/**
-	 * @param score the score to set
+	 * @param score set the game score
 	 */
 	public void setScore(int score) {
 		
 		this.score = score;
 		
 	}
-
+	
+	/**
+	 * @return the game status
+	 */
+	public int getStatus() {
+		
+		return status;
+		
+	}
+	
+	/**
+	 * @param status set the game status with one of
+	 * the status constants
+	 */
+	public void setStatus(int status) {
+		
+		this.status = status;
+		
+	}
+	
+	/**
+	 * Do everything required to start the game
+	 */
+	public void start() {
+		
+		this.setScore(0);
+		this.setStatus(ACTIVE);
+		
+	}
+	
 }
