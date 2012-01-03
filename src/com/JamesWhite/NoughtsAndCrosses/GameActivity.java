b88c50c12b43f782dedfa67e0ab79573852c5953
jameswhite <jameswhite@81d7e092-4b5a-4a12-8c2c-2554addf96e1>
2012-01-03 17:12:48 +0000

    Display a toast notification after a players go.
    git-svn-id: file:///Users/jameswhite/Dropbox/Dev/Java/Uni/assignment/branches/Assignment@16 81d7e092-4b5a-4a12-8c2c-2554addf96e1

--- src/com/JamesWhite/NoughtsAndCrosses/GameActivity.java
+++ src/com/JamesWhite/NoughtsAndCrosses/GameActivity.java
@@ -2,22 +2,24 @@ package com.JamesWhite.NoughtsAndCrosses;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends Activity implements OnClickListener {
 
 	// Define all the grid cells
 	LinearLayout cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8;
-	
+
 	// Define the player choice buttons
 	Button selectNoughts;
 	Button selectCrosses;
-	
+
 	// Define the onClick parent and child views
 	LinearLayout parent;
 	TextView child;
@@ -34,40 +36,40 @@ public class GameActivity extends Activity implements OnClickListener {
 
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.game);
-		
+
 		// Show our dialog to get the player type choice
 		final Dialog dialog = new Dialog(GameActivity.this);
 		dialog.setContentView(R.layout.gamestartdiaglog);
 		dialog.setTitle("Choose wisely!");
 		dialog.show();
-		
+
 		// Instantiate our player choice buttons and set their click listeners
 		selectNoughts = (Button) dialog.findViewById(R.id.selectNoughts);
 		selectNoughts.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				
+
 				human.setType(0);
 				computer.setType(1);
 				dialog.dismiss();
-				
+
 			}
-			
+
 		});
-		
+
 		selectCrosses = (Button) dialog.findViewById(R.id.selectCrosses);
 		selectCrosses.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				
+
 				human.setType(1);
 				computer.setType(0);
 				dialog.dismiss();
-				
+
 			}
-			
+
 		});
 
 		// Instantiate all our Cells and Cell Values
@@ -114,6 +116,14 @@ public class GameActivity extends Activity implements OnClickListener {
 			child.setText("0");
 
 		}
+
+		// Display a notification that it is now the CPU's go
+		Context context = getApplicationContext();
+		CharSequence text = "Nice move, now it's Skynet's turn!";
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
 
 	}
 
